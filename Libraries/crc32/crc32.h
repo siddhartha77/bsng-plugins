830e4acbec632a0cc5123470c538c186b9522891
siddhartha77 <77siddhartha@gmail.com>
2022-09-07 20:08:34 -0700

    Change line endings

--- Libraries/crc32/crc32.h
+++ Libraries/crc32/crc32.h
@@ -1,1 +1,2 @@
-unsigned long crc32EncodePStr(Str255 s, unsigned long init);unsigned long crc32Encode(unsigned char *bytes, unsigned long byteCount, unsigned long init);
+unsigned long crc32EncodePStr(Str255 s, unsigned long init);
+unsigned long crc32Encode(unsigned char *bytes, unsigned long byteCount, unsigned long init);
