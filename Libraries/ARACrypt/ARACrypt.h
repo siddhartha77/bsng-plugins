2639575b809a06c04574f02aa59b5ace4f06e37e
siddhartha77 <77siddhartha@gmail.com>
2023-11-04 14:50:44 -0700

    Add files via upload

---
+++ Libraries/ARACrypt/ARACrypt.h
@@ -0,0 +1,1 @@
+#ifndef __ARACRYPT_H__#define __ARACRYPT_H__void ARACryptInit();void ARACryptTransformString(unsigned char* csKey, unsigned char* csTarget, int targetLen);void ARACryptSetKey(signed char* csKey);void ARACryptTransformChar(unsigned char* cwsTarget);#endif
