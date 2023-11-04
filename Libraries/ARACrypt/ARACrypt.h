bae8f5b1cadcffb00f4e2e7193118842080c32fd
siddhartha77 <77siddhartha@gmail.com>
2023-11-04 14:51:15 -0700

    Update ARACrypt.h

--- Libraries/ARACrypt/ARACrypt.h
+++ Libraries/ARACrypt/ARACrypt.h
@@ -1,1 +1,9 @@
-#ifndef __ARACRYPT_H__#define __ARACRYPT_H__void ARACryptInit();void ARACryptTransformString(unsigned char* csKey, unsigned char* csTarget, int targetLen);void ARACryptSetKey(signed char* csKey);void ARACryptTransformChar(unsigned char* cwsTarget);#endif
+#ifndef __ARACRYPT_H__
+#define __ARACRYPT_H__
+
+void ARACryptInit();
+void ARACryptTransformString(unsigned char* csKey, unsigned char* csTarget, int targetLen);
+void ARACryptSetKey(signed char* csKey);
+void ARACryptTransformChar(unsigned char* cwsTarget);
+
+#endif
